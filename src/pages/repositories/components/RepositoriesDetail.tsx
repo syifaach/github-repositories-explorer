1b9255d9202f7cbb540671355788e6d1165d3cce
syifaach <syifachrnnsas@gmail.com>
2025-07-10 00:57:53 +0700

    fix view of repo detail

--- src/pages/repositories/components/RepositoriesDetail.tsx
+++ src/pages/repositories/components/RepositoriesDetail.tsx
@@ -1,4 +1,5 @@
 import Accordion from '../../../components/Accordion'
+import Card from '../../../components/Card'
 import { RepositoryDetailProps } from '../../../constants/interfaces/repositories'
 
 const RepositoryDetail = ({ data, detailData, onClick }: RepositoryDetailProps) => {
@@ -6,16 +7,18 @@ const RepositoryDetail = ({ data, detailData, onClick }: RepositoryDetailProps) 
         return (
             <>
                 {data?.map((val: any, idx: number) => (
-                    <div key={idx} className='ms-2'>
-                        <div className='d-flex justify-content-between align-items-center fw-bold'>
-                            <h5>{val?.repoTitle}</h5>
-                            <h5>{val?.rating} ⭐</h5>
-                        </div>
+                    <Card cardClass='p-1 mb-2' cardContent={
+                        <div key={idx} className='ms-2'>
+                            <div className='d-flex justify-content-between align-items-center fw-bold'>
+                                <h5>{val?.repoTitle}</h5>
+                                <h5>{val?.rating} ⭐</h5>
+                            </div>
 
-                        <div>
-                            <label>{val?.repoDescription}</label>
+                            <div>
+                                <label>{val?.repoDescription}</label>
+                            </div>
                         </div>
-                    </div>
+                    } />
                 ))}
             </>
         )
